351a5af4b2d7aae79827b10c97a7a6f110e8a8d4
Eric Chen <eric.chen0121@gmail.com>
2015-07-24 19:35:12 -0700

    adding npm modules for browserify and react componentry

---
+++ client/components/hello.jsx
@@ -0,0 +1,11 @@
+ var HelloUser = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user()
+    };
+  },
+  render() {
+    return <span>Hello {this.data.currentUser.username}!</span>
+  }
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+Browserify, React, React-router
+http://react-in-meteor.readthedocs.org/en/latest/client-npm/
+
+Babel
